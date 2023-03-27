f1678cbb098989a3d94367427c7e6ce050fbde16
Andrew M Almeida <am.almeida000@gmail.com>
2023-03-27 16:06:46 -0700

    working on hero background frame

---
+++ components/Background.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+function Background({}: Props) {
+  return (
+    <div className='relative flex justify-center items-center'>
+      <div className='absolute border-[#333333] rounded-full h-[200px] w-[200px] mt-52 hover:animate-ping'/>
+      <div className='absolute border-[#333333] rounded-full h-[300px] w-[300px] mt-52 animate-pulse' />
+      <div className='absolute border-[#333333] rounded-full h-[500px] w-[500px] mt-52 animate-pulse' />
+      <div className='absolute border-[#333333] rounded-full h-[650px] w-[650px] mt-52 animate-pulse' />
+      <div className='absolute border-[#333333] rounded-full h-[800px] w-[800px] mt-52 animate-pulse'/>
+    </div>
+  )
+}
+
+export default Background
