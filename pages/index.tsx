4c164eded76e65e7c0f10271c57796a79121073c
Andrew M Almeida <am.almeida000@gmail.com>
2023-03-30 11:35:57 -0700

    starting projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,11 +30,14 @@ export default function Home() {
         <Experience />
       </section>
 
-      <section className="snap-start" id="skills">
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
 
       {/* Project */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact  */}
     </div>
