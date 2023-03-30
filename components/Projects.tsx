e1f8ad63fcc7b3733f022a029ddfb770c740e25a
Andrew M Almeida <am.almeida000@gmail.com>
2023-03-30 13:08:03 -0700

    added colored section to projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type Props = {};
 
 function Projects({}: Props) {
-  return <div>Projects</div>;
+  return (
+    <div className="h-screen relative flex flex-col justify-evenly items-center overflow-hidden text-left mx-auto md:flex-row max-w-full z-0">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] "></div>
+    </div>
+  );
 }
 
 export default Projects;
