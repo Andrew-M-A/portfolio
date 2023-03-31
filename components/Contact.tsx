efba569ff926ebda95a3112bf0f246d167a6d2dd
Andrew M Almeida <am.almeida000@gmail.com>
2023-03-30 22:26:33 -0700

    created Contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,7 +3,13 @@ import React from "react";
 type Props = {};
 
 function Contact({}: Props) {
-  return <div className="h-screen">Contact</div>;
+  return (
+    <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+    </div>
+  );
 }
 
 export default Contact;
