9981a783a8a44cf7c38d5adfe097ef56cafd2bc3
Andrew M Almeida <am.almeida000@gmail.com>
2023-04-02 12:30:29 -0700

    added form section to contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,26 +9,39 @@ function Contact({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
+
       <div className="flex flex-col space-y-10">
         <h4 className="text-4xl font-semibold text-center">
           Can't wait to hear from you!{" "}
           <span className="decoration-[#F7AB0A]/50 underline">Contact Me</span>
         </h4>
-        <div className="flex justify-center items-center space-x-5">
-          <PhoneIcon className="text-[#F7AB0A] j-7 w-7 animate-pulse" />
-          <p className="text-2xl">2533802356</p>
+        <div className="space-y-10">
+          <div className="flex justify-center items-center space-x-5">
+            <PhoneIcon className="text-[#F7AB0A] j-7 w-7 animate-pulse" />
+            <p className="text-2xl">2533802356</p>
+          </div>
+          <div className="flex justify-center items-center space-x-5">
+            <EnvelopeIcon className="text-[#F7AB0A] j-7 w-7 animate-pulse" />
+            <p className="text-2xl">am.almeida000@gmail.com</p>
+          </div>
+          <div className="flex justify-center items-center space-x-5">
+            <MapPinIcon className="text-[#F7AB0A] j-7 w-7 animate-pulse" />
+            <p className="text-2xl">
+              4135 38th Street <br />
+              San Diego, California 92105
+            </p>
+          </div>
         </div>
-        <div className="flex justify-center items-center space-x-5">
-          <EnvelopeIcon className="text-[#F7AB0A] j-7 w-7 animate-pulse" />
-          <p className="text-2xl">am.almeida000@gmail.com</p>
-        </div>
-        <div className="flex justify-center items-center space-x-5">
-          <MapPinIcon className="text-[#F7AB0A] j-7 w-7 animate-pulse" />
-          <p className="text-2xl">
-            4135 38th Street <br />
-            San Diego, California 92105
-          </p>
-        </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input className="contactInput" type="text" />
+            <input className="contactInput" type="text" />
+          </div>
+          <input className="contactInput" type="text" />
+          <textarea className="contactInput" name="" id="" />
+          <button> Submit </button>
+        </form>
       </div>
     </div>
   );
