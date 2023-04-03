1c12362f6208550448e3d60ae14ad600bdc45b10
Andrew M Almeida <am.almeida000@gmail.com>
2023-04-03 16:01:14 -0700

    added With info to experience section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -18,14 +18,14 @@ function ExperienceCard({}: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.5 }}
         viewport={{ once: true }}
-        src="https://media.licdn.com/dms/image/D5603AQEv7_Y8S-_IEw/profile-displayphoto-shrink_100_100/0/1679972877520?e=1685577600&v=beta&t=LHgbtnkbVjTp5_Gl1HH3lsmCwNzIBbiVvAhQlmw0Zbo"
+        src="https://media.licdn.com/dms/image/C560BAQFWLH1S1gfBuA/company-logo_200_200/0/1675797611026?e=1688601600&v=beta&t=2-R_wED5vjH9XPnbGBXICTVwRDylyXiVH4kPnL1jkZM"
         alt="experience"
       />
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">Job Title</h4>
-        <p className="font-bold text-2xl mt-1">Company</p>
-        <div className="flex space-x-2 my-2">
-          <img
+        <h4 className="text-4xl font-light"> Web Designer </h4>
+        <p className="font-bold text-2xl mt-1">With. Company LLC</p>
+        <div className="flex justify-center items-center space-x-2 my-2">
+          {/* <img
             className="h-10 w-10 rounded-full"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
             alt=""
@@ -39,15 +39,30 @@ function ExperienceCard({}: Props) {
             className="h-10 w-10 rounded-full"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
             alt=""
-          />
+          /> */}
         </div>
-        <p className="uppercase py-5 text-gray-300"> dates </p>
+        <p className="uppercase py-5 text-gray-300"> Freelance </p>
         <ul className="list-disc space-y-4 ml-5 text-lg">
-          <li>summary</li>
-          <li>summary</li>
-          <li>summary</li>
-          <li>summary</li>
-          <li>summary</li>
+          <li>
+            Designed and developed a visually striking website to drive user
+            engagement
+          </li>
+          <li>
+            Utilized Webflow designer to bring company's ideas to life and
+            ensure that best practices were followed
+          </li>
+          <li>
+            Coordinated with client daily to ensure that all aspects of the
+            website are consistent and on brand
+          </li>
+          <li>
+            Currently working on a proposal to secure funding to teach
+            under-resourced youth web design/development
+          </li>
+          <li>
+            Provided assistance for unhoused individuals in Downtown San Diego
+            in the form of water and safe use kits
+          </li>
         </ul>
       </div>
     </article>
